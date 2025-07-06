dcccecd82f18f2f89dd366b947ee27659a4ff50a
xiao103 <zhuce3@icloud.com>
2025-07-06 15:10:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -51,7 +51,7 @@ const BLOG = {
   //CUSTOM_EXTERNAL_JS: ['']， // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
   //CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
   CUSTOM_EXTERNAL_JS: ''， // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
-  CUSTOM_EXTERNAL_CSS: '', // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
+  CUSTOM_EXTERNAL_CSS: ''， // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
 
   // 自定义菜单
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu类型的菜单，替代了3.12版本前的Page类型

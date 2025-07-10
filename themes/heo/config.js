aa868d1ac43ae17c93126f4849fa2a70ec02f725
xiao103 <zhuce3@icloud.com>
2025-07-10 19:25:35 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -27,9 +27,7 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: {title:'行业资讯',url:'/tag/文章'}，
-  HEO_HERO_CATEGORY_2: {title:'学具产品',url:'/tag/学具'}，
-  HEO_HERO_CATEGORY_3: {title:'教具产品',url:'/tag/教具'}，
+  HEO_HERO_CATEGORY_1: {title:'行业资讯',url:'/tag/文章'},HEO_HERO_CATEGORY_2: {title:'学具产品',url:'/tag/学具'},HEO_HERO_CATEGORY_3: {title:'教具产品',url:'/tag/教具'}
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
